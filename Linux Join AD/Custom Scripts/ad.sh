a12e971021f6e56b6fdce0e472cf6712fe38a8ea
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-04-09 15:22:00 -0300

    Update ad.sh

--- Linux Join AD/Custom Scripts/ad.sh
+++ Linux Join AD/Custom Scripts/ad.sh
@@ -2,3 +2,8 @@
 
 # Change Wallpaper:
 gsettings set org.kde.plasma.desktop wallpaper "/mnt/wall.jpg"
+
+WIP
+echo "blacklist usb-storage" | sudo tee /etc/modprobe.d/usb-storage.conf
+sudo update-initramfs -u
+sudo reboot
