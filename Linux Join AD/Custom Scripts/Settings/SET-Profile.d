dc79bc6af48083375bdcdecad9c2f98d58cf37cc
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-04-09 15:09:19 -0300

    Create SET-Profile.d

---
+++ Linux Join AD/Custom Scripts/Settings/SET-Profile.d
@@ -0,0 +1,7 @@
+Create /etc/profile.d/lock_wallpaper.sh
+
+#!/bin/bash
+cp /etc/skel/.config/plasma-org.kde.plasma.desktop-appletsrc ~/.config/
+chmod 444 ~/.config/plasma-org.kde.plasma.desktop-appletsrc
+
+  
