a545feac4552c5c1ef4cac38e528da077eb394f6
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-14 22:59:06 -0300

    Update 1-Backup_Total.sh

--- CDN/ReDo/1-Backup_Total.sh
+++ CDN/ReDo/1-Backup_Total.sh
@@ -1,6 +1,5 @@
-destiny="/mnt/zzz"
+destiny="/mnt/disk01/bkpsys"
 datetime=$(date +"%d_%m_%y")
-
 sudo tar -I 'lz4 -1 -c -' -cpf "$destiny"/etc-"$datetime".tar.lz4 \
     --exclude='/etc/machine-id' \
     /etc/
