870f68994c6cb10375549062781eed04b638c042
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-14 23:01:36 -0300

    Update 1-Backup_Total.sh

--- CDN/ReDo/1-Backup_Total.sh
+++ CDN/ReDo/1-Backup_Total.sh
@@ -3,3 +3,14 @@ datetime=$(date +"%d_%m_%y")
 sudo tar -I 'lz4 -1 -c -' -cpf "$destiny"/etc-"$datetime".tar.lz4 \
     --exclude='/etc/machine-id' \
     /etc/
+
+
+# 1. Exportar configuração da VM
+virsh dumpxml pfsense > pfsense-backup.xml
+# 2. Parar a VM (recomendado para consistência)
+virsh shutdown pfsense
+# 3. Copiar o arquivo qcow2
+cp /var/lib/libvirt/images/pfsense.qcow2 /backup/pfsense-backup.qcow2
+# 4. Religar a VM
+virsh start pfsense
+
