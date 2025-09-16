4eba878257ef68ee1383d30f875ea1a1085c08ab
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-16 17:14:34 -0300

    Update 2-Restore_Total.sh

--- CDN/ReDo/2-Restore_Total.sh
+++ CDN/ReDo/2-Restore_Total.sh
@@ -1,50 +1,49 @@
 #!/bin/bash
 
-datetime1=$(yq -r '.Informacoes.Data_Instalacao' "$YAML_FILE")
-[ "$datetime1" = null ] && {
-  datetime0=$(date +"%d/%m/%Y - %H:%M")
-  sudo yq -i ".Informacoes.Data_Instalacao = \"${datetime0}\"" /srv/system.yaml
-  sudo yq -i ".Informacoes.Data_Restauracao = \"Nunca foi reinstalado.\"" /srv/system.yaml
-} || {
-  
-}
-
+sudo mkdir -p /srv/containers
 sudo mkdir -p /mnt/bkpsys
 sudo mount -t ext4 LABEL=bkpsys /mnt/bkpsys
 
-Restaurar arquivos .yaml para seus devidos lugares.
+pathrestore=$(find /mnt/bkpsys -name "*.tar.lz4" 2>/dev/null | head -1 | xargs dirname)
 
-FSTAB: LABEL=sysbkp    /mnt/sysbkp    ext4    defaults,noauto    0 2
+if ! [ -f /srv/restored1.lock ]; then
+  datetime0=$(date +"%d/%m/%Y - %H:%M")
+  sudo yq -i ".Informacoes.Data_Restauracao = \"${datetime0}\"" /srv/system.yaml
 
-Encontrar todos os containers recentes:
-pathrestore=$(find /mnt/bkpsys -name "*.tar.lz4" 2>/dev/null | head -1 | xargs dirname) && echo "Pasta: $path"
+  sudo rsync -va $pathrestore/system.yaml /srv/system.yaml
+  sudo rsync -va $pathrestore/containers.yaml /srv/containers.yaml
 
-sudo mkdir -p /srv/containers
-
-find $pathrestore -type f -name "*.tar.lz4" -not -name "etc*.tar.lz4" -mtime -1 -print0 | \
-while IFS= read -r -d '' file; do
+  find $pathrestore -type f -name "*.tar.lz4" -not -name "etc*.tar.lz4" -mtime -1 -print0 | \
+  while IFS= read -r -d '' file; do
     echo "Restaurando: $file"
     sudo tar -I 'lz4 -d -c -' -xf "$file" -C "$DESTINO"
     echo "Concluído: $(basename "$file")"
-done
+  done
 
-# Fazer backup atual do /etc antes de restaurar
-echo "Criando backup de segurança do /etc atual..."
-sudo tar -I 'lz4 -1 -c -' -cpf "/etc-$datetime.tar.lz4" /etc/
-find $pathrestore -type f -name "etc*.tar.lz4" -mtime -1 -print0 | \
-while IFS= read -r -d '' file; do
-  echo "Restaurando ETC: $file"
-  sudo tar -I 'lz4 -d -c -' -xf "$file" -C /etc/
-  echo "ETC concluído: $(basename "$file")"
-done
+  # Fazer backup atual do /etc antes de restaurar
+  echo "Criando backup de segurança do /etc atual..."
+  sudo tar -I 'lz4 -1 -c -' -cpf "/etc-$datetime.tar.lz4" /etc/
 
-# 1. Parar VM se estiver rodando
-virsh destroy pfsense
-# 2. Remover VM atual
-virsh undefine pfsense
-# 3. Restaurar arquivo qcow2
-cp /backup/pfsense-backup.qcow2 /var/lib/libvirt/images/pfsense.qcow2
-# 4. Redefinir VM
-virsh define pfsense-backup.xml
-# 5. Iniciar VM
-virsh start pfsense
+  find $pathrestore -type f -name "etc*.tar.lz4" -mtime -1 -print0 | \
+  while IFS= read -r -d '' file; do
+    echo "Restaurando ETC: $file"
+    sudo tar -I 'lz4 -d -c -' -xf "$file" -C /etc/
+    echo "ETC concluído: $(basename "$file")"
+  done
+
+  sudo touch /srv/restored1.lock
+  sudo reboot
+fi
+
+if ! [ -f /srv/restored2.lock ]
+  # 1. Restaurar arquivo qcow2
+  cp /backup/pfsense-backup.qcow2 /var/lib/libvirt/images/pfsense.qcow2
+  # 2. Redefinir VM
+  virsh define pfsense-backup.xml
+  # 3. Iniciar VM
+  virsh start pfsense
+  sudo touch /srv/restored2.lock
+  sudo reboot
+fi
+
+  #FSTAB: LABEL=sysbkp    /mnt/sysbkp    ext4    defaults,noauto    0 2
