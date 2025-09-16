49d9351a1c101a2ce50cb903e75718a479bf007c
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-16 17:02:26 -0300

    Update 2-Restore_Total.sh

--- CDN/ReDo/2-Restore_Total.sh
+++ CDN/ReDo/2-Restore_Total.sh
@@ -1,38 +1,42 @@
+#!/bin/bash
 
-  datetime1=$(yq -r '.Informacoes.Data_Instalacao' "$YAML_FILE")
-  [ "$datetime1" = null ] && {
-    datetime0=$(date +"%d/%m/%Y - %H:%M")
-    sudo yq -i ".Informacoes.Data_Instalacao = \"${datetime0}\"" /srv/system.yaml
-    sudo yq -i ".Informacoes.Data_Restauracao = \"Nunca foi reinstalado.\"" /srv/system.yaml
-  } || {
-    
-  }
+datetime1=$(yq -r '.Informacoes.Data_Instalacao' "$YAML_FILE")
+[ "$datetime1" = null ] && {
+  datetime0=$(date +"%d/%m/%Y - %H:%M")
+  sudo yq -i ".Informacoes.Data_Instalacao = \"${datetime0}\"" /srv/system.yaml
+  sudo yq -i ".Informacoes.Data_Restauracao = \"Nunca foi reinstalado.\"" /srv/system.yaml
+} || {
+  
+}
 
 sudo mkdir -p /mnt/bkpsys
-
-Encontrar o bkpsys
-lsblk -f | grep bkpsys
-findfs LABEL=bkpsys
-blkid | grep bkpsys
-
-Montar bkpsys
-sudo mount LABEL="bkpsys" "$MOUNT_POINT"
+sudo mount -t ext4 LABEL=bkpsys /mnt/bkpsys
 
 Restaurar arquivos .yaml para seus devidos lugares.
+
 FSTAB: LABEL=sysbkp    /mnt/sysbkp    ext4    defaults,noauto    0 2
 
 Encontrar todos os containers recentes:
-find /mnt/dados/BACKUP_CONTAINERS/ -type f -name "*.tar.lz4" -mtime -1 -ls
-
+pathrestore=$(find /mnt/bkpsys -name "*.tar.lz4" 2>/dev/null | head -1 | xargs dirname) && echo "Pasta: $path"
 
 sudo mkdir -p /srv/containers
-find /mnt/dados/BACKUP_CONTAINERS/ -type f -name "*.tar.lz4" -mtime -1 -print0 | \
+
+find $pathrestore -type f -name "*.tar.lz4" -not -name "etc*.tar.lz4" -mtime -1 -print0 | \
 while IFS= read -r -d '' file; do
     echo "Restaurando: $file"
     sudo tar -I 'lz4 -d -c -' -xf "$file" -C "$DESTINO"
     echo "Concluído: $(basename "$file")"
 done
 
+# Fazer backup atual do /etc antes de restaurar
+echo "Criando backup de segurança do /etc atual..."
+sudo tar -I 'lz4 -1 -c -' -cpf "/etc-$datetime.tar.lz4" /etc/
+find $pathrestore -type f -name "etc*.tar.lz4" -mtime -1 -print0 | \
+while IFS= read -r -d '' file; do
+  echo "Restaurando ETC: $file"
+  sudo tar -I 'lz4 -d -c -' -xf "$file" -C /etc/
+  echo "ETC concluído: $(basename "$file")"
+done
 
 # 1. Parar VM se estiver rodando
 virsh destroy pfsense
