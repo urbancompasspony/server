b921a0411ba8c7fe9d9e59d0db6f6d8ca2dc0d10
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-22 17:01:11 -0300

    Update 1-Backup_Total.sh

--- CDN/ReDo/1-Backup_Total.sh
+++ CDN/ReDo/1-Backup_Total.sh
@@ -4,7 +4,10 @@ destiny=$(sed -n '2p' /srv/scripts/config/backupcont)
 datetime=$(date +"%d_%m_%y")
 sudo tar -I 'lz4 -1 -c -' -cpf "$destiny"/etc-"$datetime".tar.lz4 \
     --exclude='/etc/machine-id' \
+    --exclude='/etc/fstab' \
     /etc/
+
+sudo cp /etc/fstab "$destiny"/fstab-"$datetime".backup
 
 mkdir -p docker-network-backup
 for network in $(docker network ls --format "{{.Name}}" | grep -v "bridge\|host\|none"); do
