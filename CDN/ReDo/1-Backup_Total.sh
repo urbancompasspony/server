72a317c877b155c5c08de0c0e5b3bf4a61874657
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-22 17:32:36 -0300

    Update 1-Backup_Total.sh

--- CDN/ReDo/1-Backup_Total.sh
+++ CDN/ReDo/1-Backup_Total.sh
@@ -2,6 +2,7 @@
 
 destiny=$(sed -n '2p' /srv/scripts/config/backupcont)
 datetime=$(date +"%d_%m_%y")
+
 sudo tar -I 'lz4 -1 -c -' -cpf "$destiny"/etc-"$datetime".tar.lz4 \
     --exclude='/etc/machine-id' \
     --exclude='/etc/fstab' \
@@ -9,17 +10,40 @@ sudo tar -I 'lz4 -1 -c -' -cpf "$destiny"/etc-"$datetime".tar.lz4 \
 
 sudo cp /etc/fstab "$destiny"/fstab-"$datetime".backup
 
-mkdir -p docker-network-backup
+mkdir -p "$destiny"/docker-network-backup
 for network in $(docker network ls --format "{{.Name}}" | grep -v "bridge\|host\|none"); do
-    docker network inspect $network > docker-network-backup/$network.json
+  docker network inspect $network > "$destiny"/docker-network-backup/$network.json
 done
 
-# 1. Exportar configuração da VM
-datetime=$(date +%Y%m%d_%H%M%S)
-virsh dumpxml pfsense > /mnt/disk01/pfsense-vm-$datetime.xml
-# 2. Parar a VM (recomendado para consistência)
-virsh shutdown pfsense
-# 3. Copiar o arquivo qcow2
-sudo rsync -a /var/lib/libvirt/images/pfsense*.qcow2 /mnt/disk01/pfsense-backup.qcow2
-# 4. Religar a VM
-virsh start pfsense
+# Função para aguardar VM parar
+wait_vm_shutdown() {
+    local vm_name="$1"
+    local timeout=180
+    local count=0
+    while virsh list --state-running --name | grep -q "^$vm_name$"; do
+        if [ $count -ge $timeout ]; then
+            virsh destroy "$vm_name"
+            sleep 2
+            break
+        fi
+        sleep 2
+        ((count+=2))
+    done
+    echo "VM $vm_name parada"
+}
+
+virsh list --all --name | grep -i pfsense | while read vm_name; do
+    if [[ -n "$vm_name" ]] && virsh list --state-running --name | grep -q "^$vm_name$"; then
+        virsh shutdown "$vm_name"
+        wait_vm_shutdown "$vm_name"
+    fi
+done
+
+sudo find /var/lib/libvirt/images/ -iname "*pfsense*" -exec rsync -va {} "$destiny"/ \;
+
+virsh list --all --name | grep -i pfsense | while read vm_name; do
+    if [[ -n "$vm_name" ]]; then
+        virsh dumpxml "$vm_name" > "$destiny"/"$vm_name"-vm-$datetime.xml
+        virsh start "$vm_name"
+    fi
+done
