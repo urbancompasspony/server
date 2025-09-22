f9337e4cb4078ba2908d50dcef508b1516f49e1e
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-22 17:48:35 -0300

    Update 1-Backup_Total.sh

--- CDN/ReDo/1-Backup_Total.sh
+++ CDN/ReDo/1-Backup_Total.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 destiny=$(sed -n '2p' /srv/scripts/config/backupcont)
 datetime=$(date +"%d_%m_%y")
-
 # ETAPA 01
 ##########################################################################################################################
 if [ -b "$(sudo blkid -L bkpsys 2>/dev/null)" ]; then
@@ -12,23 +10,19 @@ else
   device=$(df "$mount_point" | grep -v Filesystem | awk '{print $1}')
   [[ -n "$device" ]] && sudo e2label "$device" "bkpsys" || echo "Erro: dispositivo não encontrado"
 fi
-
 # ETAPA 02
 ##########################################################################################################################
 sudo tar -I 'lz4 -1 -c -' -cpf "$destiny"/etc-"$datetime".tar.lz4 \
     --exclude='/etc/machine-id' \
     --exclude='/etc/fstab' \
     /etc/
-
 sudo cp /etc/fstab "$destiny"/fstab-"$datetime".backup
-
 # ETAPA 03
 ##########################################################################################################################
 mkdir -p "$destiny"/docker-network-backup
 for network in $(docker network ls --format "{{.Name}}" | grep -v "bridge\|host\|none"); do
-  docker network inspect $network > "$destiny"/docker-network-backup/$network.json
+  docker network inspect "$network" > "$destiny"/docker-network-backup/"$network".json
 done
-
 # ETAPA 04
 ##########################################################################################################################
 # Função para aguardar VM pfSense existente parar
@@ -47,19 +41,16 @@ wait_vm_shutdown() {
     done
     echo "VM $vm_name parada"
 }
-
-virsh list --all --name | grep -i pfsense | while read vm_name; do
+virsh list --all --name | grep -i pfsense | while read -r vm_name; do
     if [[ -n "$vm_name" ]] && virsh list --state-running --name | grep -q "^$vm_name$"; then
         virsh shutdown "$vm_name"
         wait_vm_shutdown "$vm_name"
     fi
 done
-
 sudo find /var/lib/libvirt/images/ -iname "*pfsense*" -exec rsync -va {} "$destiny"/ \;
-
-virsh list --all --name | grep -i pfsense | while read vm_name; do
+virsh list --all --name | grep -i pfsense | while read -r vm_name; do
     if [[ -n "$vm_name" ]]; then
-        virsh dumpxml "$vm_name" > "$destiny"/"$vm_name"-vm-$datetime.xml
+        virsh dumpxml "$vm_name" > "$destiny"/"$vm_name"-vm-"$datetime".xml
         virsh start "$vm_name"
     fi
 done
