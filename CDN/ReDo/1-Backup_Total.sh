bbee22fb3d9475ca692f8a62e768990a2cf85c69
Zóio The Green Eye <32694176+urbancompasspony@users.noreply.github.com>
2025-09-24 23:40:37 -0300

    Update 1-Backup_Total.sh

--- CDN/ReDo/1-Backup_Total.sh
+++ CDN/ReDo/1-Backup_Total.sh
@@ -4,9 +4,6 @@ datetime=$(date +"%d_%m_%y")
 
 # ETAPA 00
 ##########################################################################################################################
-sudo crontab -l | sudo tee "$destiny"/crontab-bkp > /dev/null
-# ETAPA 01
-##########################################################################################################################
 if [ -b "$(sudo blkid -L bkpsys 2>/dev/null)" ]; then
     :;
 else
@@ -14,6 +11,9 @@ else
   device=$(df "$mount_point" | grep -v Filesystem | awk '{print $1}')
   [[ -n "$device" ]] && sudo e2label "$device" "bkpsys" || echo "Erro: dispositivo não encontrado"
 fi
+# ETAPA 01
+##########################################################################################################################
+sudo crontab -l | sudo tee "$destiny"/crontab-bkp > /dev/null
 # ETAPA 02
 ##########################################################################################################################
 sudo tar -I 'lz4 -1 -c -' -cpf "$destiny"/etc-"$datetime".tar.lz4 \
@@ -21,41 +21,40 @@ sudo tar -I 'lz4 -1 -c -' -cpf "$destiny"/etc-"$datetime".tar.lz4 \
     --exclude='/etc/fstab' \
     /etc/
 sudo cp /etc/fstab "$destiny"/fstab-"$datetime".backup
-sudo cp /srv/*.yaml "$destiny"
 # ETAPA 03
 ##########################################################################################################################
-mkdir -p "$destiny"/docker-network-backup
+sudo mkdir -p "$destiny"/docker-network-backup
 for network in $(docker network ls --format "{{.Name}}" | grep -v "bridge\|host\|none"); do
-  docker network inspect "$network" > "$destiny"/docker-network-backup/"$network".json
+  docker network inspect "$network" | sudo tee "$destiny"/docker-network-backup/"$network".json> /dev/null
 done
 # ETAPA 04
 ##########################################################################################################################
 # Função para aguardar VM pfSense existente parar
 wait_vm_shutdown() {
-    local vm_name="$1"
-    local timeout=180
-    local count=0
-    while virsh list --state-running --name | grep -q "^$vm_name$"; do
-        if [ $count -ge $timeout ]; then
-            virsh destroy "$vm_name"
-            sleep 2
-            break
-        fi
-        sleep 2
-        ((count+=2))
-    done
-    echo "VM $vm_name parada"
+  local vm_name="$1"
+  local timeout=180
+  local count=0
+  while virsh list --state-running --name | grep -q "^$vm_name$"; do
+      if [ $count -ge $timeout ]; then
+          virsh destroy "$vm_name"
+          sleep 2
+          break
+      fi
+      sleep 2
+      ((count+=2))
+  done
+  echo "VM $vm_name parada"
 }
 virsh list --all --name | grep -i pfsense | while read -r vm_name; do
-    if [[ -n "$vm_name" ]] && virsh list --state-running --name | grep -q "^$vm_name$"; then
-        virsh shutdown "$vm_name"
-        wait_vm_shutdown "$vm_name"
-    fi
+  if [[ -n "$vm_name" ]] && virsh list --state-running --name | grep -q "^$vm_name$"; then
+      virsh shutdown "$vm_name"
+      wait_vm_shutdown "$vm_name"
+  fi
 done
 sudo find /var/lib/libvirt/images/ -iname "*pfsense*" -exec rsync -va {} "$destiny"/ \;
 virsh list --all --name | grep -i pfsense | while read -r vm_name; do
-    if [[ -n "$vm_name" ]]; then
-        virsh dumpxml "$vm_name" > "$destiny"/"$vm_name"-vm-"$datetime".xml
-        virsh start "$vm_name"
-    fi
+  if [[ -n "$vm_name" ]]; then
+    virsh dumpxml "$vm_name" | sudo tee "$destiny"/"$vm_name"-vm-"$datetime".xml> /dev/null
+    virsh start "$vm_name"
+  fi
 done
